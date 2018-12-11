406076d8620649bd8c9129b90037110084d4be7e
leader22 <y.sugiura.0316@gmail.com>
2018-12-11 14:12:29 +0900

    Parse single attribute

--- stun/attribute.ts
+++ stun/attribute.ts
@@ -6,10 +6,13 @@ import { ATTRIBUTE_TYPE } from './constants';
  *  0                   1                   2                   3
  *  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
  * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
- * |             Type             |            Length              |
+ * |             Type             |            Length(byte)        |
  * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
  * |                        Value (variable)                    ....
  * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *
+ * Value is N * 32bit w/ padding bit
+ * TODO: impl
  *
  */
 export function createSoftware(): Buffer {
--- stun/message.ts
+++ stun/message.ts
@@ -5,7 +5,7 @@ import { numberToStringWithRadixAndPadding } from './utils';
 export function createBindingRequest(): Buffer {
   const body = Buffer.concat([
     // SHOULD
-    createSoftware()
+    createSoftware(),
   ]);
 
   // body size is needed for message length
@@ -20,4 +20,37 @@ export function isStunMessage(msg: Buffer): boolean {
   const first8bit = numberToStringWithRadixAndPadding(first1byte, 2, 8);
 
   return first8bit.charAt(0) === '0' && first8bit.charAt(1) === '0';
+}
+
+interface Attribute {}
+export function parseAttributes(msg: Buffer): Attribute[] {
+  // STUN Message Header is 20byte = 160bit
+  // const header = msg.slice(0, 20);
+  const body = msg.slice(20, msg.length);
+  console.log(`body: ${msg.length - 20}byte(${(msg.length - 20) * 8}bit)`);
+
+  let offset = 0;
+  while (offset < body.length) {
+    const type = body.readUInt16BE(offset);
+    offset += 2; // 16bit = 2byte
+
+    const length = body.readUInt16BE(offset);
+    offset += 2; // 16bit = 2byte
+
+    const value = body.slice(offset, offset + length);
+    offset += length;
+
+    // STUN Attributes are in 32bit(4byte) boundary
+    const missingBoundaryByte = length % 4;
+    const paddingByte = missingBoundaryByte === 0 ? 0 : 4 - missingBoundaryByte;
+    offset += paddingByte;
+
+    console.log('------');
+    console.log(`type: 0x${numberToStringWithRadixAndPadding(type, 16, 4)}`);
+    console.log(`length: ${length}byte`);
+    console.log(`value: ${value.toString('hex')}`);
+    console.log('------');
+  }
+
+  return [];
 }
