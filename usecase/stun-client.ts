012a09caf176a6934f5e1c3baeb51b6794e58f02
leader22 <y.sugiura.0316@gmail.com>
2018-12-11 12:36:45 +0900

    Update usecase

--- usecase/stun-client.ts
+++ usecase/stun-client.ts
@@ -1,12 +1,15 @@
 import * as dgram from 'dgram';
-import { createBindingRequest } from '../stun';
+import { createBindingRequest, isStunMessage } from '../stun';
 
 const socket = dgram.createSocket({ type: 'udp4' });
-
 socket.on('message', (msg: Buffer) => {
-  console.log('message from server');
-  // TODO: これをdecodeすれば、自分のIPとかがわかるはず
-  console.log(msg.toString('hex'));
+  if (!isStunMessage(msg)) {
+    console.log('not a stun packet', msg.toString('hex'));
+    socket.close();
+    return;
+  }
+
+  console.log('recv', msg.toString('hex'));
   socket.close();
 });
 socket.bind(12345);
