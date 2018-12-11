fb449f63a10244d65568a5db8d055c56c360a518
leader22 <y.sugiura.0316@gmail.com>
2018-12-11 12:36:20 +0900

    Add comment

--- stun/message.ts
+++ stun/message.ts
@@ -15,7 +15,9 @@ export function createBindingRequest(): Buffer {
 }
 
 export function isStunMessage(msg: Buffer): boolean {
+  // 8bit is enough to know first 0, 1bit
   const first1byte = msg.readUInt8(0);
   const first8bit = numberToStringWithRadixAndPadding(first1byte, 2, 8);
+
   return first8bit.charAt(0) === '0' && first8bit.charAt(1) === '0';
 }
