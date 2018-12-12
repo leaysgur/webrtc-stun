5db1acb708c806c8591d56b5f79823a53858ecd6
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-12-12 22:51:49 +0900

    Parse transaction ID

--- stun/internal/header.ts
+++ stun/internal/header.ts
@@ -4,14 +4,10 @@ export interface Header {
   type: number;
   length: number;
   magicCookie: number;
-  // TODO: transactionId
+  transactionId: Buffer;
 }
 
 /*
- * - STUN Message
- *   - ヘッダ（20byte）
- *   - ボディ（0~Nbyte: 任意の数のAttrbutes）
- *
  * STUN Message Header
  *
  *  0                   1                   2                   3
@@ -28,27 +24,29 @@ export interface Header {
  *
  */
 function create(messageType: number, attrByte: number): Buffer {
-  // STUN Message Header is 20byte = 160bit
-  const header = Buffer.alloc(20);
+  // STUN Message Header is 20byte(8+12) = 160bit
+  const header = Buffer.alloc(8);
+
   // 000 - 015bit(16bit = 2byte): Message type
   header.writeUInt16BE(messageType, 0);
   // 016 - 032bit(16bit = 2byte): Message length
   header.writeUInt16BE(attrByte, 2);
   // 033 - 064bit(32bit = 4byte): Magic cookie
   header.writeUInt32BE(0x2112a442, 4);
-  // 065 - 160bit(96bit = 12byte): Transaction id
-  const transactionId = crypto.randomBytes(12).toString('hex');
-  header.write(transactionId, 8, 12);
 
-  return header;
+  // 065 - 160bit(96bit = 12byte): Transaction id
+  const transactionId = crypto.randomBytes(12);
+
+  return Buffer.concat([header, transactionId]);
 }
 
 function parse(header: Buffer): Header {
   const type = header.readUInt16BE(0);
   const length = header.readUInt16BE(2);
   const magicCookie = header.readUInt32BE(4);
+  const transactionId = header.slice(8, 20);
 
-  return { type, length, magicCookie };
+  return { type, length, magicCookie, transactionId };
 }
 
 export default { create, parse }
