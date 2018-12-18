43ff7d7afb5206b965e5027071c90522b2163425
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-12-18 18:02:47 +0900

    Fix exports

--- stun/index.ts
+++ stun/index.ts
@@ -1,13 +1,5 @@
-// import { Header } from './header';
-// import attributes, { Attributes } from './internal/attributes';
 import { numberToStringWithRadixAndPadding } from './utils';
 
-// interface Message {
-//   header: Header;
-//   attrs: Attributes;
-// }
-
-// export * from './reader';
 export function isStunMessage(msg: Buffer): boolean {
   // 8bit is enough to know first and second bit
   const first1byte = msg.readUInt8(0);
@@ -16,17 +8,7 @@ export function isStunMessage(msg: Buffer): boolean {
   return first8bit.charAt(0) === '0' && first8bit.charAt(1) === '0';
 }
 
-// export function parseStunMessage(msg: Buffer): Message {
-//   // STUN Message Header is 20byte
-//   // so, rest of part is Attributes
-//   return {
-//     header: header.parse(msg.slice(0, 20)),
-//     attrs: attributes.parse(msg.slice(20, msg.length)),
-//   };
-// }
-
 export * from './message';
 export * from './header';
 export * from './attribute/software';
 export * from './message-type';
-// export * from './attribute-type';
