c82e2aaef1d127f311932e8f3b66dc2003e04fa8
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-12-20 17:06:13 +0900

    Cosme examples

--- examples/stun-client.ts
+++ examples/stun-client.ts
@@ -16,28 +16,23 @@ import {
 const socket = dgram.createSocket({ type: 'udp4' });
 
 socket.on('message', (msg: Buffer) => {
+  // ignore non-related packets
   if (!isStunMessage(msg)) {
     console.log('not a stun packet', msg.toString('hex'));
-    socket.close();
     return;
   }
 
   const { header, body } = parseStunMessage(msg);
 
-  switch (header.type) {
-    case STUN_MESSAGE_TYPE.BINDING_RESPONSE_SUCCESS:
-      const xorMappedAddress = body.find(
-        i => i.type === STUN_ATTRIBUTE_TYPE.XOR_MAPPED_ADDRESS,
-      );
+  if (header.type === STUN_MESSAGE_TYPE.BINDING_RESPONSE_SUCCESS) {
+    const xorMappedAddress = body.find(
+      i => i.type === STUN_ATTRIBUTE_TYPE.XOR_MAPPED_ADDRESS,
+    );
 
-      if (xorMappedAddress) {
-        const attr = xorMappedAddress as XorMappedAddressAttribute;
-        console.log(`Your IP is ${attr.payload.address}`);
-      }
-      break;
-    case STUN_MESSAGE_TYPE.BINDING_RESPONSE_ERROR:
-      console.log('BINDING_RESPONSE_ERROR');
-      break;
+    if (xorMappedAddress) {
+      const attr = xorMappedAddress as XorMappedAddressAttribute;
+      console.log(`Your IP is ${attr.payload.address}`);
+    }
   }
 
   socket.close();
