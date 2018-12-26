fd9f98d03bd5aa539e8ad7d97fc1533ab65e44c1
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-12-26 17:13:04 +0900

    Fix client example

--- examples/stun-client.js
+++ examples/stun-client.js
@@ -1,46 +1,30 @@
+const pkg = require('../package.json');
 const dgram = require('dgram');
-const {
-  // STUN Message creator
-  isStunMessage,
-  createStunMessage,
-  parseStunMessage,
-  // combine these to create STUN message
-  Header,
-  SoftwareAttribute,
-  XorMappedAddressAttribute,
-  // constants to process
-  STUN_MESSAGE_TYPE,
-  STUN_ATTRIBUTE_TYPE,
-} = require('..');
+const { StunMessage } = require('..');
 
 const socket = dgram.createSocket({ type: 'udp4' });
 
 socket.on('message', msg => {
-  // ignore non-related packets
-  if (!isStunMessage(msg)) {
-    console.log('not a stun packet', msg.toString('hex'));
-    return;
-  }
+  const stunMessage = new StunMessage();
 
-  const { header, body } = parseStunMessage(msg);
-
-  if (header.type === STUN_MESSAGE_TYPE.BINDING_RESPONSE_SUCCESS) {
-    const xorMappedAddress = body.find(
-      i => i.type === STUN_ATTRIBUTE_TYPE.XOR_MAPPED_ADDRESS,
-    );
-
-    if (xorMappedAddress) {
-      const attr = xorMappedAddress; // as XorMappedAddressAttribute;
-      console.log(`Your IP is ${attr.payload.address}`);
+  // true if msg is valid STUN message
+  if (stunMessage.loadBuffer(msg)) {
+    // true if STUN message has BINDING_RESPONSE_SUCCESS as its type
+    if (stunMessage.isBindingResponseSuccess()) {
+      const attr = stunMessage.getXorMappedAddressAttribute();
+      // if STUN message includes attr
+      if (attr) {
+        console.log(attr);
+      }
     }
   }
 
   socket.close();
 });
 
-const packet = createStunMessage({
-  header: new Header(STUN_MESSAGE_TYPE.BINDING_REQUEST),
-  body: [new SoftwareAttribute('webrtc-stack-study')],
-});
-socket.send(packet, 3478, 'stun.webrtc.ecl.ntt.com');
-// socket.send(packet, 19302, 'stun.l.google.com');
+const packet = new StunMessage()
+  .setBindingRequestType()
+  .setSoftwareAttribute(`${pkg.name}@${pkg.version}`)
+  .toBuffer();
+// socket.send(packet, 3478, 'stun.webrtc.ecl.ntt.com');
+socket.send(packet, 19302, 'stun.l.google.com');
